245a4c976496cf5b9f5303ce2bd31e20ac8a11a9
Marco Antonio Sugamele <sugamele.marco@gmail.com>
2013-05-24 23:59:44 -0300

    Refactoring tests, now using blocks

--- spec/libs/fileManager/fileReader/file_system_reader_spec.rb
+++ spec/libs/fileManager/fileReader/file_system_reader_spec.rb
@@ -9,50 +9,35 @@ describe FileSystemReader do
   context "Reading files new change log", :reading_new_files => true do
     
     before(:all) do
-      @file  = Tempfile.new(['20130513','.txt'])    
-      @file1 = Tempfile.new(['20130514','.txt'])
-      @file2 = Tempfile.new(['20130515','.txt'])
-      @file3 = Tempfile.new(['20130513','.yml'])
+      file = lambda do |amount_text, amount_yml| 
+        array_files = Array.new
+        (1..amount_text).each { |n| array_files.push(Tempfile.new(["#{n}",'.txt'])) }
+        (1..amount_yml).each { |n| array_files.push(Tempfile.new(["#{n}",'.yml'])) }
+        array_files
+      end
+      @array = file.call(3,1)
     end
 
     after(:all) do
-      @file.close
-      @file1.close
-      @file2.close
-      @file3.close    
-      @file.unlink
-      @file1.unlink
-      @file2.unlink
-      @file3.unlink
-
-      yml = YAML.load_file("spec/changeLogConfigTest.yml")
-      yml["lastReaderFile"] = nil
-      File.open("spec/changeLogConfigTest.yml", 'w+') {|f| f.write(yml.to_yaml) }
+      @array.each { |file| file.close file.unlink }
+      yaml_fixed_last_reader_file(nil)      
     end
 
-    it "No file was read, we have to return all txt" do
-      
-       expect(FileSystemReader.get_files_change_log("/tmp", "txt").size).to eq(3)
-       
+    it "No file was read, we have to return all txt" do      
+       expect(FileSystemReader.get_files_change_log("/tmp", "txt").size).to eq(3)       
     end
 
     it "The last time the file is smaller than those created, return all" do
-      
       date = DateTime.new
-      yml = YAML.load_file("spec/changeLogConfigTest.yml")
-      yml["lastReaderFile"] = (date-10).to_time
-      File.open("spec/changeLogConfigTest.yml", 'w+') {|f| f.write(yml.to_yaml) }
-      expect(FileSystemReader.get_files_change_log("/tmp", "txt").size).to eq(3)
-      
+      yaml_fixed_last_reader_file((date-10).to_time)
+      expect(FileSystemReader.get_files_change_log("/tmp", "txt").size).to eq(3)      
     end
 
     it "We have a new file, return one data" do
       
-      FileUtils.touch @file3.path, :mtime => Time.new + 60*60*12
-      yml = YAML.load_file("spec/changeLogConfigTest.yml")
-      yml["lastReaderFile"] = @file3.mtime
-      File.open("spec/changeLogConfigTest.yml", 'w+') {|f| f.write(yml.to_yaml) }
-
+      FileUtils.touch @array[2].path, :mtime => Time.new + 60*60*12
+      yml = yaml_fixed_last_reader_file(@array[2].mtime)
+      
       @file4 = Tempfile.new(['20130516','.txt'])
       FileUtils.touch @file4.path, :mtime => Time.new + 60*60*24
       expect(FileSystemReader.get_files_change_log("/tmp", "txt", yml["lastReaderFile"]).size).to eq(1)
@@ -60,31 +45,41 @@ describe FileSystemReader do
       @file4.close
       @file4.unlink
       
+    end
+
+    private
+    def yaml_fixed_last_reader_file(value)
+      yaml = YAML.load_file("spec/changeLogConfigTest.yml")
+      yaml["lastReaderFile"] = value
+      File.open("spec/changeLogConfigTest.yml", 'w+') {|f| f.write(yaml.to_yaml) }
+      yaml      
     end    
   end
 
   context "Get the contents of files", :contents => true do
     
     before(:all) do
-      @file  = Tempfile.new(['20130513','.txt'])
-      @file.write("Hello Word")
-      @file.rewind
-      
-      @file1 = Tempfile.new(['20130514','.txt'])
-      @file1.write("BugFix: Fixed error in some class\r\nFeature: Some feature here\r\nTests: é assim ó")
-      @file1.rewind
+      content_file = ["Hello Word", "BugFix: Fixed error in some class\r\nFeature: Some feature here\r\nTests: é assim ó"]
+      files = lambda do |content_file|          
+          array_files = Array.new          
+          content_file.each do |content, index|
+            file  = Tempfile.new(["#{index}",'.txt'])
+            file.write(content)
+            file.rewind
+            array_files.push(file)
+          end
+          array_files
+      end
+      @files = files.call(content_file)
     end
 
     after(:all) do
-      @file.close
-      @file1.close
-      @file.unlink
-      @file1.unlink
+     @files.each { |file| file.close file.unlink}
     end
     
     it "Get the contents of files" do
-      expect(FileSystemReader.get_content @file).to eq("Hello Word")
-      expect((FileSystemReader.get_content @file1)).to eq("BugFix: Fixed error in some class\r\nFeature: Some feature here\r\nTests: é assim ó")
+      expect(FileSystemReader.get_content @files[0]).to eq("Hello Word")
+      expect((FileSystemReader.get_content @files[1])).to eq("BugFix: Fixed error in some class\r\nFeature: Some feature here\r\nTests: é assim ó")
     end
     
   end
--- spec/libs/threads/pool_threads_spec.rb
+++ spec/libs/threads/pool_threads_spec.rb
@@ -7,86 +7,29 @@ describe PoolThreads do
   context "Do merge the contents of the files using threads" do
     
     before(:all) do
-      @file  = Tempfile.new(['201305131','.txt'])
-      @file.write("- Description fixeds and bugfix\r\nfile1")
-      @file.rewind
-      
-      @file1 = Tempfile.new(['201305142','.txt'])
-      @file1.write("- Description fixeds and bugfix\r\nfile2")
-      @file1.rewind
-      
-      @file2 = Tempfile.new(['201305153','.txt'])
-      @file2.write("- Description fixeds and bugfix\r\nfile3")
-      @file2.rewind
-      
-      @file3 = Tempfile.new(['201305144','.txt'])
-      @file3.write("- Description fixeds and bugfix\r\nfile4")
-      @file3.rewind
-      
-      @file4 = Tempfile.new(['201305155','.txt'])
-      @file4.write("- Description fixeds and bugfix\r\nfile5")
-      @file4.rewind
-      
-      @file5 = Tempfile.new(['201305146','.txt'])
-      @file5.write("- Description fixeds and bugfix\r\nfile6")
-      @file5.rewind
-      
-      @file6 = Tempfile.new(['201305157','.txt'])
-      @file6.write("- Description fixeds and bugfix\r\nfile7")
-      @file6.rewind
-      
-      @file7 = Tempfile.new(['201305148','.txt'])
-      @file7.write("- Description fixeds and bugfix\r\nfile8")
-      @file7.rewind
-      
-      @file8 = Tempfile.new(['201305159','.txt'])
-      @file8.write("- Description fixeds and bugfix\r\nfile9")
-      @file8.rewind
-      
-      @file9 = Tempfile.new(['2013051411','.txt'])
-      @file9.write("- á é à ç ü \r\nfile10")
-      @file9.rewind
+      file = lambda do  
+        array_files = Array.new
+        (1..9).each do |n| 
+          temp_file = Tempfile.new(["#{n}",'.txt'])
+          temp_file.write("- Description fixeds and bugfix\r\nfile#{n}")
+          temp_file.rewind
+          array_files.push(temp_file) 
+        end
+        temp_file = Tempfile.new(['2013051411','.txt'])
+        temp_file.write("- á é à ç ü \r\nfile#{array_files.size+1}")
+        temp_file.rewind
+        array_files.push(temp_file)
+        array_files
+      end
+      @array_files = file.call
     end
 
     after(:all) do
-      @file.close
-      @file1.close
-      @file2.close
-      @file3.close
-      @file4.close
-      @file5.close
-      @file6.close
-      @file7.close
-      @file8.close
-      @file9.close
-      
-      @file.unlink
-      @file1.unlink
-      @file2.unlink
-      @file3.unlink
-      @file4.unlink
-      @file5.unlink
-      @file6.unlink
-      @file7.unlink
-      @file8.unlink
-      @file9.unlink
-      
+      @array_files.each { |file| file.close file.unlink }
     end
    
    it "The merge should contain the contents of all files" do
-      files = Array.new
-      files.push(@file)
-      files.push(@file1)
-      files.push(@file2)
-      files.push(@file3)
-      files.push(@file4)
-      files.push(@file5)
-      files.push(@file6)
-      files.push(@file7)
-      files.push(@file8)
-      files.push(@file9)
-      
-      contents = PoolThreads.new files
+      contents = PoolThreads.new @array_files
       contents.run_limit_threads 9
       merge_contents = contents.get_merge_content_files
 
