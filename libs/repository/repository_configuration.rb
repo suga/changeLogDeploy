86cb70e0dcc73fa487c350c108955ed2d4c20d9d
Marco Antonio Sugamele <sugamele.marco@gmail.com>
2013-05-25 01:44:27 -0300

    Removed the dependencies yaml reader

--- libs/repository/repository_configuration.rb
+++ libs/repository/repository_configuration.rb
@@ -1,32 +1,31 @@
-require File.dirname(__FILE__) + "/../fileManager/fileReader/yaml_reader"
+require File.dirname(__FILE__) + "/../fileManager/configuration_file"
 
 module RepositoryConfiguration
   attr_reader :path_file_yaml, :config_yml
   
   def configuration_repository(path_change_log_configuration) 
-    @config_yml = YamlReader.new(path_change_log_configuration)
-    @config_yml.configurations
+    @config_yml = ConfigurationFile.new(path_change_log_configuration)
     self
   end
   
   def get_path_change_log_deploy
-    @config_yml.configurations['changeLogPath']
+    @config_yml.change_log_path
   end  
   
   def get_last_reader_file
-    @config_yml.configurations['lastReaderFile']
+    @config_yml.last_reader_file
   end
   
   def get_notification_emails
-    @config_yml.configurations['notificationEmails']
+    @config_yml.notification_emails
   end
   
   def get_file_extension
-    @config_yml.configurations['extensionFiles']
+    @config_yml.extension_files
   end
   
   def get_limit_threads
-    @config_yml.configurations['limitThreads']
+    @config_yml.limit_threads
   end
   
 end
